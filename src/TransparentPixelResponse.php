4aa079c8bbec2768a7a12a8deb6a844e60696106
Adam Gąsowski <adam.gasowski@gander.pl>
2015-08-22 21:54:44 +0200

    fix class, requirements, and phpDoc

--- src/TransparentPixelResponse.php
+++ src/TransparentPixelResponse.php
@@ -6,31 +6,32 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * Class TransparentPixelResponse
  * @package Gander\Tools\TransparentPixelResponse
+ * @author Luciano Mammino <lucianomammino@gmail.com>
  * @author Adam Gąsowski <adam.gasowski@gander.pl>
  */
 class TransparentPixelResponse extends Response
 {
     /**
-     * Base 64 encoded contents for 1px transparent gif and png
+     * Base 64 encoded contents for 1px transparent gif
      * @var string
      */
-    const PIXEL_CONTENT =
-        'R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+    const IMAGE_CONTENT =
+        'R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw=='
     ;
 
     /**
      * The response content type
      * @var string
      */
-    const PIXEL_CONTENT_TYPE = 'image/gif';
+    const CONTENT_TYPE = 'image/gif';
 
     /**
      * Constructor
      */
     public function __construct()
     {
-        parent::__construct(base64_decode(static::PIXEL_CONTENT));
-        $this->headers->set('Content-Type', static::PIXEL_CONTENT_TYPE);
+        parent::__construct(base64_decode(static::IMAGE_CONTENT));
+        $this->headers->set('Content-Type', static::CONTENT_TYPE);
         $this->setPrivate();
         $this->headers->addCacheControlDirective('no-cache', true);
         $this->headers->addCacheControlDirective('must-revalidate', true);
